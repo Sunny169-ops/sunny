764b47fc10796ac4378bac45ccce7bc65db42496
Sunvish <51359852+Sunvish@users.noreply.github.com>
2020-05-20 20:13:06 +0530

    Add all my files

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <img src="logo.png"></img>
+      </header>
+      <div className="main">
+        <div className="main-prize">
+          <img src="1.png"></img>
+        </div>
+        <div className="main-article">
+          <h4>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h4>
+          <ul>
+            <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+          </ul>
+          <ul>
+          <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
+          </ul>
+          <div className="main-photo">
+            <img src="2.png"></img>
+          </div>
+          <div className="main-article-2">
+            <p>Government of India has awarded the <b>"National Energy Conservation Award 2018"</b>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+          </div>
+        </div>
+      </div>
+      <div className="main-2">
+        <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+      </div>
+      <div className="main-tool">
+       <div className="main-small-tool">
+         <img src="a.png"></img>
+       </div>
+       <div className="main-small-tool-1">
+         <img src="b.png"></img>
+         <img src="c.png"></img>
+         <img src="d.png"></img>
+         <img src="e.png"></img>
+         <img src="f.png"></img>
+         <img src="g.png"></img>
+         <img src="h.png"></img>
+         <img src="i.png"></img>
+         <img src="j.png"></img>
+         <img src="k.png"></img>
+         <img src="l.png"></img>
+         <img src="m.png"></img>
+         <img src="n.png"></img>
+         <img src="p.png"></img>
+         <img src="q.png"></img>
+         <img src="r.png"></img>
+       </div>
+       
+      </div>
+      <div className="main-small-article">
+         <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motor</p>
+        </div>
+        <div className="footer-article">
+          <h3>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h3>
+          <div className="footer-small-article">
+          <div className="footer-small-article-1">
+          <h1>CHEMICALS & PROCESS</h1> 
+          </div>
+          <div className="footer-small-article-2">
+          <h1>POWER</h1>
+          </div>
+          <div className="footer-small-article-3">
+          <h1>WATER & WASTE WATER</h1> 
+          </div>
+          <div className="footer-small-article-4">
+          <h1>OILS & GAS</h1>
+          </div>
+          <div className="footer-small-article-5">
+          <h1>PHARMA</h1>
+          </div>
+          <div className="footer-small-article-6">
+          <h1>SUGARS & DISTILLERIES</h1>
+          </div>
+          <div className="footer-small-article-7">
+          <h1>PAPER & PULP</h1>
+          </div>
+          <div className="footer-small-article-8">
+         <h1> MARINE & DEFENCE</h1>
+          </div>
+          <div className="footer-small-article-9">
+         <h1>METAL & MINING </h1> 
+          </div>
+          <div className="footer-small-article-10">
+         <h1>FOOD & BEVERAGE</h1> 
+          </div>
+          <div className="footer-small-article-11">
+          <h1>PETROCHEMICAL & REFINERIES</h1>
+          </div>
+          <div className="footer-small-article-12">
+          <h1>SOLAR</h1>
+          </div>
+          <div className="footer-small-article-13">
+          <h1>BUILDING</h1>
+          </div>
+          <div className="footer-small-article-14">
+          <h1>HVAC</h1>
+          </div>
+          <div className="footer-small-article-15">
+          <h1>FIRE FIGHTING</h1>
+          </div>
+          <div className="footer-small-article-16">
+          <h1>AGRICULTURE & RESIDENTIAL</h1>
+          </div>
+          </div>
+        </div>
+        <footer>
+          <ul>
+            <li><a href=""><i class="fa fa-phone-square" aria-hidden="true">Toll free  1800 200 1234</i></a></li>
+            <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true">www.facebook.com/cripumps</i></a></li>
+            <li><a href=""><i class="fa fa-globe" aria-hidden="true">www.crigroups.com</i></a></li>
+          </ul>
+        </footer>
+      
+      
+    </div>
+  );
+}
+
+export default App;
